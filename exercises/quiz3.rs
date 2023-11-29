ba12d494a93ebcd1addf401d86d7807c68719cef
kris007iron <kris007.iron@gmail.com>
2023-11-29 13:32:34 +0000

    Refactor some_func to accept a trait object

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,44 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+pub trait GradeType {
+    fn print(&self) -> String;
+}
 
-pub struct ReportCard {
+pub struct NumericGrade {
     pub grade: f32,
+}
+
+impl GradeType for NumericGrade {
+    fn print(&self) -> String {
+        format!("achieved a grade of {}", &self.grade)
+    }
+}
+
+pub struct AlphabeticGrade {
+    pub grade: String,
+}
+
+impl GradeType for AlphabeticGrade {
+    fn print(&self) -> String {
+        format!("achieved a grade of {}", &self.grade)
+    }
+}
+
+pub struct ReportCard<T: GradeType> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: GradeType> ReportCard<T> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - {}",
+            &self.student_name,
+            &self.student_age,
+            self.grade.print()
+        )
     }
 }
 
@@ -37,27 +63,28 @@ mod tests {
 
     #[test]
     fn generate_numeric_report_card() {
-        let report_card = ReportCard {
-            grade: 2.1,
+        let numeric_report_card = ReportCard {
+            grade: NumericGrade { grade: 2.1 },
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
         assert_eq!(
-            report_card.print(),
+            numeric_report_card.print(),
             "Tom Wriggle (12) - achieved a grade of 2.1"
         );
     }
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
+        let alphabetic_report_card = ReportCard {
+            grade: AlphabeticGrade {
+                grade: "A+".to_string(),
+            },
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
         assert_eq!(
-            report_card.print(),
+            alphabetic_report_card.print(),
             "Gary Plotter (11) - achieved a grade of A+"
         );
     }
