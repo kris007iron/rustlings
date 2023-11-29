90b34d12ea2ecbc752048d1ef961b5a9b3f72322
kris007iron <kris007.iron@gmail.com>
2023-11-29 11:03:49 +0000

    Fix error handling in code

--- exercises/13_error_handling/errors4.rs
+++ exercises/13_error_handling/errors4.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -17,7 +15,17 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm... Why is this always returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        // Because the value is always positive
+        // Because the value is always nonzero
+        // Because the error variants are never constructed
+        // Because the function is not implemented correctly
+        if value > 0 {
+            Ok(PositiveNonzeroInteger(value as u64))
+        } else if value == 0 {
+            Err(CreationError::Zero)
+        } else {
+            Err(CreationError::Negative)
+        }
     }
 }
 
